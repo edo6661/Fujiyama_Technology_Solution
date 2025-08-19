a6beab6460a6efbdc373cdbb9e91a499287ef392
Tegar Saputra <tegar0651@gmail.com>
2025-08-19 15:55:53 +0700

    add: google map

---
+++ resources/views/components/features/home/map.blade.php
@@ -0,0 +1,29 @@
+<section id="office-location" class="relative px-4 py-16 bg-gray-50">
+    <div class="mx-auto">
+        <!-- Title -->
+        <h3 class="text-lg sm:text-2xl font-bold mb-6 text-green-custom text-center sm:text-left">
+            Office Location
+        </h3>
+
+        <!-- Map -->
+        <div class="w-full h-64 sm:h-80 bg-transparent rounded-lg overflow-hidden shadow-lg">
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.671136651886!2d106.78509397610306!3d-6.174763644527437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7002212556b%3A0xd553386b470af88!2sNEO%20SOHO%20APARTEMENT!5e0!3m2!1sid!2sid!4v1753431520442!5m2!1sid!2sid"
+                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade" class="rounded-lg">
+            </iframe>
+        </div>
+
+        <!-- Description & Link -->
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mt-4 gap-2">
+            <p class="text-xs sm:text-sm text-gray-600">
+                Click the map for directions to Neo Soho Central Park
+            </p>
+            <a href="https://www.google.com/maps/search/Neo+Soho" target="_blank"
+                class="text-green-custom hover:text-green-hover transition-colors text-xs sm:text-sm flex items-center gap-1 font-medium">
+                <i class="fas fa-external-link-alt"></i>
+                Open in Google Maps
+            </a>
+        </div>
+    </div>
+</section>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
 @vite(['resources/js/app.js'])
 
 <x-layouts.app title="Home">
-    <x-features.home.hero-revision/>
-    <x-features.home.about-revision/>
-    <x-features.home.what-we-do/>
-    <x-features.home.projects/>
-    <x-features.home.why-choose-us/>
+    <x-features.home.hero-revision />
+    <x-features.home.about-revision />
+    <x-features.home.what-we-do />
+    <x-features.home.projects />
+    <x-features.home.why-choose-us />
+
     {{-- <x-features.home.banner-consultation/> --}}
     {{-- <x-features.home.team/> --}}
     {{-- <x-features.home.blogs :blogs="$blogs"/> --}}
-    <x-features.home.contact/>
+    <x-features.home.contact />
+    <x-features.home.map />
 </x-layouts.app>
